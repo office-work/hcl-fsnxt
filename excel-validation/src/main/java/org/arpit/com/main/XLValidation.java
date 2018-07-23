82661b52094a88d32d7d7ca06b38401429e35317
arpit <itsarpitgupta@gmail.com>
2018-07-23 07:16:37 +0530

    excel validation

---
+++ excel-validation/src/main/java/org/arpit/com/main/XLValidation.java
@@ -0,0 +1,33 @@
+package org.arpit.com.main;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.hssf.usermodel.DVConstraint;
+import org.apache.poi.hssf.usermodel.HSSFDataValidation;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.DataValidation;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddressList;
+
+public class XLValidation {
+	public static void main(String args[]) throws FileNotFoundException {
+		Workbook workbook = new HSSFWorkbook();
+		Sheet sheet = workbook.createSheet("Data Validation");
+		CellRangeAddressList addressList = new CellRangeAddressList(0, 0, 0, 0);
+		DVConstraint dvConstraint = DVConstraint
+				.createExplicitListConstraint(new String[] { "10", "20", "30" });
+		DataValidation dataValidation = new HSSFDataValidation(addressList,
+				dvConstraint);
+		dataValidation.setSuppressDropDownArrow(true);
+		sheet.addValidationData(dataValidation);
+		FileOutputStream fileOut = new FileOutputStream("xls/XLValidation.xls");
+		try {
+			workbook.write(fileOut);
+			fileOut.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
